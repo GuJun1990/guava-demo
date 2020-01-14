bd7de08119cafd8c0636ae0ea3058c318a789f20
gujun <gujun@qiyi.com>
2020-01-14 19:27:49 +0800

    add splitter demo.

---
+++ src/main/java/com/example/guava/DemoSplitter.java
@@ -0,0 +1,18 @@
+package com.example.guava;
+
+import com.google.common.base.Splitter;
+import com.google.common.collect.Lists;
+
+/**
+ * @author gujun@qiyi.com
+ * @since 2020/1/14 6:14 下午
+ */
+public class DemoSplitter {
+
+    public static void main(String[] args) {
+        String string = "1,\n2,  3, 4,    5";
+        System.out.println(string);
+        System.out.println(Lists.newArrayList(string.split(","))); // 不会处理控制字符串
+        System.out.println(Splitter.on(",").trimResults().omitEmptyStrings().split(string)); // 处理控制字符串。
+    }
+}
