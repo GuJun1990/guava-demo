28d7e1801f4b2ba22c7d2db2c18a7a1b93e5be7a
gujun <gujun@qiyi.com>
2020-05-29 17:00:02 +0800

    update preconditions

---
+++ src/main/java/com/example/guava/preconditions/DemoPreconditions.java
@@ -0,0 +1,34 @@
+package com.example.guava.preconditions;
+
+import org.junit.Test;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+/**
+ * @author gujun@qiyi.com
+ * @since 2020/5/29 4:26 下午
+ */
+public class DemoPreconditions {
+
+    @Test
+    public void check_argument() {
+        fun1(1);
+        fun1(-1); // java.lang.IllegalArgumentException: Argument is -1, but expected non negative
+    }
+
+    @Test
+    public void check_argument_2() {
+        fun2(1, 2);
+        fun2(2, 1); // java.lang.IllegalArgumentException: Expected i < j, but 2 >= 1
+    }
+
+    void fun1(int i) {
+        checkArgument(i >= 0, "Argument is %s, but expected non negative", i);
+    }
+
+    void fun2(int i, int j) {
+        checkArgument(i < j, "Expected i < j, but %s >= %s", i, j);
+    }
+
+
+}
---
+++ src/main/java/com/example/guava/preconditions/README.md
@@ -0,0 +1,13 @@
+## Preconditions 介绍
+
+### Preconditions
+
+Guava提供了许多前提条件检查实用程序，强烈建议静态导入这些。
+```java
+import static com.google.common.base.Preconditions.checkArgument;
+```
+每种方法都有三种变体：
+1. 没有多余的参数。引发任何异常而不会出现错误消息。
+2. 一个额外的Object参数。错误消息object.toString()引发任何异常。
+3. 一个额外的String参数，带有任意数量的其他Object参数。行为类似于`printf`，但是为了GWT的兼容性和效率，它仅允许`％s`指示符。
+    * 注意：`checkNotNull`，`checkArgument`和`checkState`具有大量重载，这些重载采用原始参数和Object参数而不是varargs数组的组合-这允许上述调用避免大多数情况下避免原始装箱和varags数组分配。
